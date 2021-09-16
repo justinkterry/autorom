149554d266522caf89f62708bbe457396c53920d
Jesse Farebrother <jessefarebro@gmail.com>
2021-09-16 18:10:05 -0400

    Support for downloading ROMs at install-time, ale-py plugin support

--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
 import setuptools
+import os
+
+from setuptools.command.install import install
+
+from AutoROM import main
+
+here = os.path.dirname(os.path.abspath(__file__))
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+class InstallCommand(install):
+    user_options = install.user_options + [
+        ('accept-license', None, 'I accept the license')
+    ]
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.accept_license = None
+
+    def finalize_options(self):
+        install.finalize_options(self)
+        assert self.accept_license is None or self.accept_license == 1
+
+    def run(self):
+        install_dir = os.path.join(here, "AutoROM", "roms")
+        if self.accept_license == 1:
+            main(self.accept_license, install_dir, True)
+
+        install.run(self)
 
 setuptools.setup(
     name="AutoROM",
@@ -21,7 +48,13 @@ setuptools.setup(
         'importlib-resources; python_version < "3.9"',
     ],
     entry_points={
-        "console_scripts": ["AutoROM=AutoROM:main"],
+        "console_scripts": ["AutoROM=AutoROM:cli"],
+        "ale_py.roms": ["AutoROM=AutoROM.roms:export"]
+    },
+    zip_safe=False,
+    cmdclass={"install": InstallCommand},
+    package_data={
+        "AutoROM.roms": ["*.bin"]
     },
     python_requires=">=3.6",
     classifiers=[
